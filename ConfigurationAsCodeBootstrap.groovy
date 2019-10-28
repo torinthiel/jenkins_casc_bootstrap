c56000f253de09be9d7aa78164c1ca12ee7bfa1f
Torinthiel <torinthiel@torinthiel.pl>
2019-10-28 20:57:17 +0100

    Split the functionality between classes
    Split the limited existing functionality between several classes.
    This will serve as an entry point for creating tests for separate
    functionalities.

--- ConfigurationAsCodeBootstrap.groovy
+++ ConfigurationAsCodeBootstrap.groovy
@@ -1,17 +1,48 @@
-import java.util.logging.Logger;
-import com.bettercloud.vault.VaultConfig;
-import com.bettercloud.vault.Vault;
-import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey
-import com.cloudbees.plugins.credentials.CredentialsScope
-import com.cloudbees.plugins.credentials.domains.Domain
-
 import static java.util.logging.Level.INFO
 
-public class CascBootstrap {
-	Logger log = Logger.getLogger(getClass().name)
+import java.util.logging.Logger;
 
-	private updateCredentials(String id, String user, String privateKey) {
-		def store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey
+import com.cloudbees.plugins.credentials.CredentialsScope
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider
+import com.cloudbees.plugins.credentials.domains.Domain
+
+import jenkins.model.Jenkins;
+
+class VaultAccessor {
+	Vault vault;
+
+	VaultAccessor() {
+		VaultConfig config = new VaultConfig()
+		.address("http://172.17.0.3:8200")
+		.build();
+
+		vault = new Vault(config);
+
+		// Authenticate
+		String token = vault.auth().loginByUserPass("jenkins", "S3cRet", "userpass").getAuthClientToken();
+		config.token(token).build();
+	}
+
+	String getValue(String key) {
+		def data = vault.logical().read("secret/jenkins/config").getData()
+
+		return data.get(key)
+	}
+}
+
+class CredentialUpdater {
+	Jenkins instance;
+
+	CredentialUpdater(Jenkins instance) {
+		this.instance = instance
+	}
+
+	void updateCredentials(String id, String user, String privateKey) {
+		SystemCredentialsProvider plugin = instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0]
+		def store = plugin.getStore()
 		def globalDomain = Domain.global()
 		def newKey = new BasicSSHUserPrivateKey(
 				CredentialsScope.GLOBAL,
@@ -29,23 +60,20 @@ public class CascBootstrap {
 		else
 			store.addCredentials(globalDomain, newKey)
 	}
+}
 
-	public main(String[] ignored) {
-		VaultConfig config = new VaultConfig()
-			.address("http://172.17.0.3:8200")
-			.build();
+public class CascBootstrap {
+	Logger log = Logger.getLogger(getClass().name)
 
-		Vault vault = new Vault(config);
+	public main() {
+		VaultAccessor accessor = new VaultAccessor();
 
-		// Authenticate
-		String token = vault.auth().loginByUserPass("jenkins", "S3cRet", "userpass").getAuthClientToken();
-		config.token(token).build();
-
-		def data = vault.logical().read("secret/jenkins/config").getData()
-
-		String value = data.get("path")
+		String value = accessor.getValue("path")
 		log.log(INFO, "Retrieved from vault: " + value);
 
-		updateCredentials("ssh-key", "git", data.get("sshKey"))
+		CredentialUpdater updater = new CredentialUpdater(Jenkins.instance)
+		updater.updateCredentials("ssh-key", "git", accessor.getValue("sshKey"))
 	}
 }
+
+new CascBootstrap().main()
