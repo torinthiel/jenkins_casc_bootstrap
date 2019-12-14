20057a732f12e9ce445fba073155bc7b15d5b527
Torinthiel <torinthiel@torinthiel.pl>
2019-12-14 15:56:05 +0100

    Change quoting
    Change the way DSL script is quoted, so that I can later add some
    variables inside.

--- src/main/groovy/ConfigJobCreator.groovy
+++ src/main/groovy/ConfigJobCreator.groovy
@@ -5,9 +5,9 @@ class ConfigJobCreator {
 	// Note that this script is interpreted TWICE by groovy:
 	// Once when the class is parsed, second time when the script
 	// is passed to DslScriptLoader and executed.
-	// Because of this, the ''' inside this string have to be escaped
-	// or use """, but each $ inside needs to be escaped by double-backslash.
-	private static final String JOB_DSL_SCRIPT = '''\
+	// That's the reason some things are escaped with a single \ and some with
+	// a double one, depending which level we need to protect from.
+	private static final String JOB_DSL_SCRIPT = """\
 		def configJob = job('config') {
 			label('master')
 			scm {
@@ -24,21 +24,21 @@ class ConfigJobCreator {
 			}
 
 			steps {
-				shell("""\\
-					TARGET_DIR=\\$JENKINS_HOME/jenkins_casc_configuration
+				shell('''\\
+					TARGET_DIR=\$JENKINS_HOME/jenkins_casc_configuration
 
-					mkdir -p \\$TARGET_DIR
-					cp *.yaml \\$TARGET_DIR
-				""".stripIndent())
-				systemGroovyCommand("""\\
+					mkdir -p \$TARGET_DIR
+					cp *.yaml \$TARGET_DIR
+				'''.stripIndent())
+				systemGroovyCommand('''\\
 					import jenkins.model.Jenkins
 					def jcacPlugin = Jenkins.instance.getExtensionList(io.jenkins.plugins.casc.ConfigurationAsCode.class).first()
 					jcacPlugin.configure("/var/jenkins_home/jenkins_casc_configuration")
-				""".stripIndent())
+				'''.stripIndent())
 			}
 		}
 		queue(configJob)
-	'''.stripIndent()
+	""".stripIndent()
 
 	public generateJobs() {
 		System.out.println(JOB_DSL_SCRIPT)
